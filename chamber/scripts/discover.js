6885ccc4d7f41c181e0e2868d2729f053ed98b8a
onepercent-web <keyami2019@gmail.com>
2024-10-17 02:02:23 +0900

    saved

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -15,3 +15,23 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+
+// Visitor message in sidebar
+const visitorMessage = document.getElementById("visitor-message");
+const lastVisit = localStorage.getItem("lastVisit");
+const now = Date.now();
+
+if (lastVisit) {
+    const daysSinceLastVisit = Math.floor((now - lastVisit) / (1000 * 60 * 60 * 24));
+    if (daysSinceLastVisit < 1) {
+        visitorMessage.textContent = "Back so soon! Awesome!";
+    } else if (daysSinceLastVisit === 1) {
+        visitorMessage.textContent = `You last visited ${daysSinceLastVisit} day ago.`;
+    } else {
+        visitorMessage.textContent = `You last visited ${daysSinceLastVisit} days ago.`;
+    }
+} else {
+    visitorMessage.textContent = "Welcome! Let us know if you have any questions.";
+}
+
+localStorage.setItem("lastVisit", now);
